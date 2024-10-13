8a74218832b2ffc4af4a2b8d512d1ed63f55b301
hamazoon <hamazoon1@gmail.com>
2024-10-13 18:55:11 +0100

    print default value and data type

--- HELLOGO.go
+++ HELLOGO.go
@@ -6,10 +6,10 @@ func main(){
 
 	fmt.Println("Hello, World!")//print steatment
 
-	var Location string = "tamesna"// declaration of string varibel
+	var Location string = "tamesna"// declaration of string varibele
 	fmt.Println(Location)
 
 	var number int =32
-	fmt.Println(Location , number)//declaration of integer varibel 
+	fmt.Println(Location , number)//declaration of integer varibele 
 
 }
---
+++ print_format.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var age int
+	var check bool 
+	var price uint64
+	fmt.Println(age ,  check , price ) // print default value 
+
+	s := 45 // other assingment of a varible
+	fmt.Println(s)
+
+	var a =37
+	var b =3.14 
+	var c =true 
+	var d = "go"
+	fmt.Printf("%T , %T , %T , %T \n " , a , b , c , d ) //print data type 
+
+}
