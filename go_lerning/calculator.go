4ff9ec658b0c927f9b486b917cbcd26b4326bf4d
hamazoon <hamazoon1@gmail.com>
2024-10-19 21:01:37 +0100

    calculator by go

---
+++ go_lerning/calculator.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main () {
+
+	var a int = 10
+	var b int = 5
+	
+	var sum = a+b 
+	fmt.Printf("the sum of a and b is %d\n" , sum)
+
+	var mul = a*b 
+	fmt.Printf("the multiplication of a and b is %d\n" , mul)
+
+	var div = a/b 
+	fmt.Printf(" the divition of a by b is  %d\n" , div)
+
+	var min = a-b 
+	fmt.Printf("a minece b is %d\n" , min)
+
+}
