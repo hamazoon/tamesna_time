4f0c01312a50b64e7a5e08bc0c7fda774eaf266d
hamazoon <hamazoon1@gmail.com>
2024-11-20 21:12:17 +0100

    condition

--- bash_learning/04_condition.sh
+++ bash_learning/04_condition.sh
@@ -7,7 +7,7 @@ rand=$(( $RANDOM % 2 ))
 echo "Chose a number between (1/0)"
 read num
 
-if [[ rand == num ]]; then
+if [[ $rand == $num ]]; then
 	echo "Great you are done the level 1 "
 else
 	echo "You are died" 
@@ -19,7 +19,7 @@ echo "Welcome to the level 2 chose a number between 0 and 4 (0-4)"
 read num1
 rand1=$(( $RANDOM % 5))
 
-if [[ rand1 == num1 ]]; then 
+if [[ $rand1 == $num1 ]]; then 
 	echo "Good job you are done the level 2"
 else
 	echo "You are died"
@@ -31,7 +31,7 @@ echo "Welcome to the last level chose a number between 0 and 9 (0-9)"
 read num2
 rand2=$(( $RANDOM % 10))
 
-if [[ rand2 == num2 ]]; then 
+if [[ $rand2 == $num2 ]]; then 
 	echo "Congratulation you are win"
 else
 	echo "You are died"
