0f3620f0bf2eb3e4c92dafa060a99e22ec29ebd7
hamazoon <hamazoon1@gmail.com>
2024-11-20 21:09:12 +0100

    condition

--- bash_learning/04_condition.sh
+++ bash_learning/04_condition.sh
@@ -1,14 +1,40 @@
 #! /bin/bash
 
-echo " welcome to the digital game win or die"
+echo " Welcome to the digital game win or die"
 
 rand=$(( $RANDOM % 2 ))
 
-echo " chose a number between (1/0)"
+echo "Chose a number between (1/0)"
 read num
 
 if [[ rand == num ]]; then
-	echo " congratulation you are win "
+	echo "Great you are done the level 1 "
 else
-	echo " you are died"
+	echo "You are died" 
+	exit 1
 fi
+
+sleep 2
+echo "Welcome to the level 2 chose a number between 0 and 4 (0-4)"
+read num1
+rand1=$(( $RANDOM % 5))
+
+if [[ rand1 == num1 ]]; then 
+	echo "Good job you are done the level 2"
+else
+	echo "You are died"
+	exit 2
+fi
+
+sleep 2
+echo "Welcome to the last level chose a number between 0 and 9 (0-9)"
+read num2
+rand2=$(( $RANDOM % 10))
+
+if [[ rand2 == num2 ]]; then 
+	echo "Congratulation you are win"
+else
+	echo "You are died"
+fi
+
+
