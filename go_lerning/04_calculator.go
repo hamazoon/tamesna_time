09aaa886739c9d38fcf317de0c136dd97a80d40f
hamazoon <hamazoon1@gmail.com>
2024-12-10 09:30:42 +0100

    calculator

--- go_lerning/04_calculator.go
+++ go_lerning/04_calculator.go
@@ -2,21 +2,46 @@ package main
 
 import "fmt"
 
-func main () {
+func sum(a , b int)  int {
+	sum := a + b 
+	return sum
+}
 
-	var a int = 10
-	var b int = 5
+func sub(c , d  int)  int {
+	sub := c - d 
+	return sub 
+}
+
+func mul(e , f  int ) int {
+	mul := e * f 
+	return mul
+}
+
+func div (x , y  int) int {
+	div := x / y 
+	return div 
 	
-	var sum = a+b 
-	fmt.Printf("the sum of a and b is %d\n" , sum)
+}
 
-	var mul = a*b 
-	fmt.Printf("the multiplication of a and b is %d\n" , mul)
+func mod(m , n  int) int {
+	mod := m % n 
+	return mod 
+	
+}
 
-	var div = a/b 
-	fmt.Printf(" the divition of a by b is  %d\n" , div)
-
-	var min = a-b 
-	fmt.Printf("a mince b is %d\n" , min)
-
+func main () {
+	fmt.Println(sum(5,6))
+	fmt.Println(sum(4,-7))
+	fmt.Println(sub(4,3))
+	fmt.Println(sub(-3,-5))
+	fmt.Println(sub(5,-9))
+	fmt.Println(mul(3,5))
+	fmt.Println(mul(4,-2))
+	fmt.Println(mul(-7,-5))
+	fmt.Println(div(4,2))
+	fmt.Println(div(5,-3))
+	fmt.Println(div(-7,-9))
+	fmt.Println(mod(6,5))
+	fmt.Println(mod(-9,4))
+	fmt.Println(mod(-5,-8))
 }
