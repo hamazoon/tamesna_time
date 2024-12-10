109a31f578c0c87f31da01f30304e5dfce108ffa
hamazoon <hamazoon1@gmail.com>
2024-12-10 15:58:17 +0100

    revers a string

---
+++ go_lerning/109_rev_str.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func rev(str string) string {
+	var s string
+	for i := len(str)-1 ; i >= 0 ; i-- {
+		s = s + string(str[i])
+	}
+	return s
+}
+
+func main()  {
+	r := rev("cat")
+	z := rev("ca t")
+	x := rev("hamza")
+	fmt.Println(r)
+	fmt.Println(z)
+	fmt.Println(x)
+}
