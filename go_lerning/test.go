f2f554ec3973e5bed47ba28164809fe0021f6640
hamazoon <hamazoon1@gmail.com>
2024-12-18 18:26:24 +0100

    test

--- go_lerning/test.go
+++ go_lerning/test.go
@@ -2,16 +2,8 @@ package main
 
 import "fmt"
 
-func ToUpper(s string) string {
-	letre := []rune(s)
-	for i, char := range letre {
-		if char >= 'a' && char <= 'z' {
-			letre[i] -= 32
-		}
-	}
-	return string(letre)
-}
+
 
 func main() {
-	fmt.Println(ToUpper("Hello! How are you?"))
+	fmt.Println("Hello! How are you?")
 }
